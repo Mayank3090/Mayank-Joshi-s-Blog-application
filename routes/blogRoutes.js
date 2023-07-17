b306e680358952d3e9182eae5f279114f85e4a85
Mayank3090 <119277687+Mayank3090@users.noreply.github.com>
2023-07-17 19:07:54 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,7 +6,7 @@ const {
   getBlogByIdController,
   deleteBlogController,
   userBlogControlller,
-} = require("../controllers/blogControlller");
+} = require("../controller/blogControlller");
 
 //router object
 const router = express.Router();
